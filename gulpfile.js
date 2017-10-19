6300ae1a53567854ba8c2d4c25278e83592fb0f8
Grant Inna <grantinna@mail.ru>
2017-10-19 13:53:54 +0300

    Image min

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,38 @@
 var gulp = require( 'gulp' ),
-    smartgrid = require('smart-grid'),
     groupMedia = require('gulp-group-css-media-queries'),
     autoprefixer = require('gulp-autoprefixer'),
     cleanCSS = require('gulp-clean-css'),
     rename = require("gulp-rename"),
     jsmin = require('gulp-jsmin'),
+    imagemin = require('gulp-imagemin'),
     sass = require("gulp-sass"),
     jade = require( 'gulp-jade' ),
     concat = require('gulp-concat');
 
 
-/* It's principal settings in smart grid project */
-var GridSettings = {
-    outputStyle: 'sass', /* less || scss || sass || styl */
-    columns: 12, /* number of grid columns */
-    offset: '30px', /* gutter width px || % */
-    mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
-    container: {
-        maxWidth: '1200px', /* max-width în very large screen */
-        fields: '30px' /* side fields */
-    },
-    breakPoints: {
-        lg: {
-            width: '1200px', /* -> @media (max-width: 1200px) */
-        },
-        md: {
-            width: '960px'
-        },
-        sm: {
-            width: '780px',
-            fields: '15px' /* set fields only if you want to change container.fields */
-        },
-        xs: {
-            width: '560px'
-        }
-        /*
-         We can create any quantity of break points.
-
-         some_name: {
-         width: 'Npx',
-         fields: 'N(px|%|rem)',
-         offset: 'N(px|%|rem)'
-         }
-         */
-    }
-};
-
-/*gulp.task( 'smart-grid', function() {
-    smartgrid('dev/sass/', GridSettings);
-});*/
-
 
 // Tasks ===============================================================================================================
 
+// Images __________________________________________________________________
+
+gulp.task('compress', function() {
+    return gulp.src('dev/img/*.{png,jpg}')
+        .pipe(imagemin())
+        .pipe(gulp.dest('app/img/'));
+});
+
+
 // CSS _____________________________________________________________________
+
 gulp.task('CSS', function() {
     return gulp.src('dev/scss/main.scss')
-        .pipe(groupMedia())
+        //.pipe(groupMedia())
         .pipe(sass().on('error', sass.logError)) // Turn scss file into css
         .pipe(autoprefixer({browsers: ['last 5 versions', '> 3%']}))
         .pipe(rename("style.css"))
         .pipe(gulp.dest('app/css/'));
 });
-gulp.task('minCss', function() {
+gulp.task('minCSS', function() {
     return gulp.src('app/css/style.css')
         .pipe(cleanCSS())
         .pipe(rename("style.min.css"))
@@ -97,15 +67,16 @@ gulp.task( 'jade_template', function() {
 
 
 gulp.task( 'watch_jade', function() {
-    gulp.watch( "./app/jade/**/*.jade" , ['jade_pages']);
+    gulp.watch( "dev/jade/**/*.jade" , ['jade_pages']);
 });
 
 
 // Tasks arrays ========================================================================================================
 
-var tasksCSS = ['CSS', 'minCSS', 'watch_min', 'watch_CSS'];
+var tasksCSS = ['compress', 'CSS', 'minCSS', 'watch_min', 'watch_CSS'];
+var tasksCSS = ['compress', 'CSS', 'minCSS', 'watch_min', 'watch_CSS'];
 
-var tasksJade = ['jade_pages', 'jade_static', 'jade_template', 'watch_jade'];
+var tasksJade = ['jade_pages', 'watch_jade'];
 
 // Main tasks
 
